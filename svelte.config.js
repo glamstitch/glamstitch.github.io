4048159eeeca922cf248a46543cb3ac05da9fe2a
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-10-08 11:07:49 +0700

    Fix: Update base path for custom domain and add CNAME

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 export default {
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : (process.env.BASE_PATH || '/glamstitch-company-profile')
+      base: ''
     },
     alias: {
       $components: 'src/components',
